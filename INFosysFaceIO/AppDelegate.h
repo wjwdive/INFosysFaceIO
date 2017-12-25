61a3f8c4ee45e97dbfc59250e33ebb3ab5f4814c
wjwdive <wjwdive@163.com>
2017-12-25 17:04:16 +0800

    Initial Commit

---
+++ INFosysFaceIO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  INFosysFaceIO
+//
+//  Created by jiangwei.wang on 2017/12/25.
+//  Copyright © 2017年 jiangwei.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ INFosysFaceIO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  INFosysFaceIO
+//
+//  Created by jiangwei.wang on 2017/12/25.
+//  Copyright © 2017年 jiangwei.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
