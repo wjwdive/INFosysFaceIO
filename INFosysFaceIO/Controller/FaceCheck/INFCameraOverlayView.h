0ba65c11b3b279b2923a012a1505bf52b4e90a84
wjwdive <wjwdive@163.com>
2017-12-28 14:27:49 +0800

    can take photo without animation

---
+++ INFosysFaceIO/Controller/FaceCheck/INFCameraOverlayView.h
@@ -0,0 +1,13 @@
+//
+//  INFCamerOverlayView.h
+//  INFosysFaceIO
+//
+//  Created by jiangwei.wang on 2017/12/27.
+//  Copyright © 2017年 jiangwei.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface INFCameraOverlayView : UIView
+
+@end
---
+++ INFosysFaceIO/Controller/FaceCheck/INFCameraOverlayView.m
@@ -0,0 +1,21 @@
+//
+//  INFCamerOverlayView.m
+//  INFosysFaceIO
+//
+//  Created by jiangwei.wang on 2017/12/27.
+//  Copyright © 2017年 jiangwei.wang. All rights reserved.
+//
+
+#import "INFCameraOverlayView.h"
+
+@implementation INFCameraOverlayView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
